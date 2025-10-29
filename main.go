165418e17fb08769d37f92c032c391e7b789ae02
satyam <satyamvatsal257@gmail.com>
2025-10-29 20:13:39 +0530

    updated main

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/satyamvatsal/go-programs/vector"
-)
-
 func main() {
-	v := vector.NewVector[int](1)
-	for i := range 10 {
-		v.PushBack(i)
-		fmt.Println(v.Size)
-	}
-	for _ = range 4 {
-		v.PopBack()
-	}
-	fmt.Printf("%+v\n", v)
+	// used for testing
 }
